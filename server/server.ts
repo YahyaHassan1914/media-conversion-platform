6ff4c7984fb2ce699a75636c874cb537c6cb4a7c
YahyaHassan1914 <yahyahassanmohamed1914@gmail.com>
2026-05-30 10:19:10 +0300

    feat(frontend, backend): initialize Vite React app and Express TS server

---
+++ server/server.ts
@@ -0,0 +1,8 @@
+import app from "./src/app";
+import "dotenv/config";
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+});
